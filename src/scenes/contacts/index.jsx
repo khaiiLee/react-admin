3158949a5308ada6868084ac40d1ef3987a4e84b
KhaiLee <lequangkhaiuit@gmail.com>
2023-03-21 11:09:17 +0700

    commit

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,103 +1,97 @@
 import { Box } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
-import { mockDataContacts } from "../../data/mockData";
-import Header from "../../components/Header";
-import { useTheme } from "@mui/material";
+// import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+// import { tokens } from "../../theme";
+import React, { useState } from "react";
+// import { mockDataContacts } from "../../data/mockData";
+// import Header from "../../components/Header";
+// import { useTheme } from "@mui/material";
+import axios from "axios";
+import { Component } from "react";
+// import { render } from "preact/compat";
 
-const Contacts = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+// const Contact = () => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
 
-  const columns = [
-    { field: "id", headerName: "ID", flex: 0.5 },
-    { field: "registrarId", headerName: "Registrar ID" },
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      cellClassName: "name-column--cell",
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 1,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
-    {
-      field: "address",
-      headerName: "Address",
-      flex: 1,
-    },
-    {
-      field: "city",
-      headerName: "City",
-      flex: 1,
-    },
-    {
-      field: "zipCode",
-      headerName: "Zip Code",
-      flex: 1,
-    },
-  ];
+//   const [ data, setData ] = useState([]);
 
-  return (
-    <Box m="20px">
-      <Header
-        title="CONTACTS"
-        subtitle="List of Contacts for Future Reference"
-      />
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${colors.grey[100]} !important`,
-          },
-        }}
-      >
-        <DataGrid
-          rows={mockDataContacts}
-          columns={columns}
-          components={{ Toolbar: GridToolbar }}
-        />
-      </Box>
-    </Box>
-  );
-};
+//   componentDidMount = async () => {
+//     await axios.get(`https://bill-be.onrender.com/mssql`).then(res => {
+//       setData(res.data);
+//     });
+//     console.log(data);
+//   };
 
+//   return (
+//     <Box m="20px">
+//       <Header
+//         title="CONTACTS"
+//         subtitle="List of Contacts for Future Reference"
+//       />
+//       <Box
+//         m="40px 0 0 0"
+//         height="75vh"
+//         sx={{
+//           "& .MuiDataGrid-root": {
+//             border: "none",
+//           },
+//           "& .MuiDataGrid-cell": {
+//             borderBottom: "none",
+//           },
+//           "& .name-column--cell": {
+//             color: colors.greenAccent[300],
+//           },
+//           "& .MuiDataGrid-columnHeaders": {
+//             backgroundColor: colors.blueAccent[700],
+//             borderBottom: "none",
+//           },
+//           "& .MuiDataGrid-virtualScroller": {
+//             backgroundColor: colors.primary[400],
+//           },
+//           "& .MuiDataGrid-footerContainer": {
+//             borderTop: "none",
+//             backgroundColor: colors.blueAccent[700],
+//           },
+//           "& .MuiCheckbox-root": {
+//             color: `${colors.greenAccent[200]} !important`,
+//           },
+//           "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+//             color: `${colors.grey[100]} !important`,
+//           },
+//         }}
+//       >
+//         {/* <DataGrid
+//           rows={mockDataContacts}
+//           columns={columns}
+//           components={{ Toolbar: GridToolbar }}
+//         /> */}
+//       </Box>
+//     </Box>
+//   );
+// };
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+  async componentDidMount() {
+    await axios
+      .get(`https://bill-be.onrender.com/mssql`, {
+        params: {
+          query: "select top 100 * from dbo.react_app",
+          token: 123456,
+        },
+      })
+      .then(res => {
+        this.setState({ data: res.data });
+      });
+    console.log(this.state.data);
+  }
+  render() {
+    return <Box />;
+  }
+}
 export default Contacts;
--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,80 +1,192 @@
+// import { Box } from "@mui/material";
+// import Header from "../../components/Header";
+// import React, { useState } from "react";
+// import MaterialTable from "@material-table/core";
+// import * as XLSX from "xlsx";
+// import axios from "axios";
+
+// const EXTENSIONS = [ "xlsx", "xls", "csv" ];
+
+// const ImportFile = () => {
+//   const [ colDefs, setColDefs ] = useState();
+//   const [ data, setData ] = useState();
+
+//   const getExention = file => {
+//     const parts = file.name.split(".");
+//     const extension = parts[parts.length - 1];
+//     return EXTENSIONS.includes(extension); // return boolean
+//   };
+
+//   const convertToJson = (headers, data) => {
+//     const rows = [];
+//     data.forEach(row => {
+//       let rowData = {};
+//       row.forEach((element, index) => {
+//         rowData[headers[index]] = element;
+//       });
+//       rows.push(rowData);
+//     });
+//     return rows;
+//   };
+
+//   const importExcel = e => {
+//     const file = e.target.files[0];
+//     const reader = new FileReader();
+//     reader.onload = event => {
+//       //parse data
+//       const bstr = event.target.result;
+//       const workBook = XLSX.read(bstr, { type: "binary" });
+
+//       //get first sheet
+//       const workSheetName = workBook.SheetNames[0];
+//       const workSheet = workBook.Sheets[workSheetName];
+//       //convert to array
+//       const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+//       const headers = fileData[0];
+//       const heads = headers.map(head => ({ title: head, field: head }));
+//       setColDefs(heads);
+
+//       //removing header
+//       fileData.splice(0, 1);
+
+//       setData(convertToJson(headers, fileData));
+
+//       // Push data to server
+//       axios
+//         .post("https://bill-be.onrender.com/insert-mssql", {
+//           query:
+//             "INSERT INTO dbo.uit" +
+//             "(" +
+//             Object.keys(data[0]) +
+//             ")" +
+//             "VALUES" +
+//             "('" +
+//             Object.values(data[0]).join("','") +
+//             "')",
+//           token: 123456,
+//         })
+//         .then(response => {
+//           console.log(response.data);
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         });
+//     };
+
+//     if (file) {
+//       if (getExention(file)) {
+//         reader.readAsBinaryString(file);
+//       } else {
+//         alert("Invalid file input, Select Excel, CSV file");
+//       }
+//     } else {
+//       setData([]);
+//       setColDefs([]);
+//     }
+//   };
+
+//   return (
+//     <Box m="20px">
+//       <Header title="IMPORT" subtitle="Import XLSX or CSV" />
+//       <Box>
+//         <div className="App">
+//           <input type="file" onChange={importExcel} />
+//           <MaterialTable title="Import Table" data={data} columns={colDefs} />
+//         </div>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default ImportFile;
+
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
-import React, { useState } from 'react';
-import MaterialTable from '@material-table/core';
-import * as XLSX from 'xlsx'
+import React, { useState } from "react";
+import MaterialTable from "@material-table/core";
+import * as XLSX from "xlsx";
+import axios from "axios";
 
-const EXTENSIONS = ['xlsx', 'xls', 'csv']
+const EXTENSIONS = [ "xlsx", "xls", "csv" ];
+
 const ImportFile = () => {
-  const [colDefs, setColDefs] = useState()
-  const [data, setData] = useState()
+  const [ colDefs, setColDefs ] = useState([]);
+  const [ data, setData ] = useState([]);
 
-  const getExention = (file) => {
-    const parts = file.name.split('.')
-    const extension = parts[parts.length - 1]
-    return EXTENSIONS.includes(extension) // return boolean
-  }
+  const getExtension = file => {
+    const parts = file.name.split(".");
+    const extension = parts[parts.length - 1];
+    return EXTENSIONS.includes(extension);
+  };
 
   const convertToJson = (headers, data) => {
-    const rows = []
+    const rows = [];
     data.forEach(row => {
-      let rowData = {}
+      let rowData = {};
       row.forEach((element, index) => {
-        rowData[headers[index]] = element
-      })
-      rows.push(rowData)
-
+        rowData[headers[index]] = element;
+      });
+      rows.push(rowData);
     });
-    return rows
-  }
+    return rows;
+  };
 
-  const importExcel = (e) => {
-    const file = e.target.files[0]
+  const importExcel = async e => {
+    const file = e.target.files[0];
+    if (file && getExtension(file)) {
+      const reader = new FileReader();
+      reader.onload = async event => {
+        //parse data
+        const bstr = event.target.result;
+        const workBook = XLSX.read(bstr, { type: "binary" });
 
-    const reader = new FileReader()
-    reader.onload = (event) => {
-      //parse data
+        //get first sheet
+        const workSheetName = workBook.SheetNames[0];
+        const workSheet = workBook.Sheets[workSheetName];
+        //convert to array
+        const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+        const headers = fileData[0];
+        const heads = headers.map(head => ({ title: head, field: head }));
+        console.log(heads);
+        console.log(fileData);
+        setColDefs(heads);
 
-      const bstr = event.target.result
-      const workBook = XLSX.read(bstr, { type: "binary" })
+        //removing header
+        fileData.splice(0, 1);
 
-      //get first sheet
-      const workSheetName = workBook.SheetNames[0]
-      const workSheet = workBook.Sheets[workSheetName]
-      //convert to array
-      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
-      // console.log(fileData)
-      const headers = fileData[0]
-      const heads = headers.map(head => ({ title: head, field: head }))
-      setColDefs(heads)
-
-      //removing header
-      fileData.splice(0, 1)
-
-
-      setData(convertToJson(headers, fileData))
-    }
-
-    if (file) {
-      if (getExention(file)) {
-        reader.readAsBinaryString(file)
-      }
-      else {
-        alert("Invalid file input, Select Excel, CSV file")
-      }
+        const jsonData = convertToJson(headers, fileData);
+        setData(jsonData);
+        // Push data to server
+        try {
+          const columns = Object.keys(jsonData[0]);
+          const values = jsonData
+            .map(item => Object.values(item).join("','"))
+            .join("'),('");
+          const query = `INSERT INTO dbo.uit (${columns}) VALUES ('${values}')`;
+          const response = await axios.post(
+            "https://bill-be.onrender.com/insert-mssql",
+            { query, token: 123456 }
+          );
+          console.log(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      reader.readAsBinaryString(file);
     } else {
-      setData([])
-      setColDefs([])
+      setData();
+      setColDefs();
+      alert("Invalid file input. Please select an Excel, CSV file");
     }
-  }
+  };
 
   return (
     <Box m="20px">
       <Header title="IMPORT" subtitle="Import XLSX or CSV" />
       <Box>
         <div className="App">
-        <input type="file" onChange={importExcel} />
-        <MaterialTable title="Import Table" data={data} columns={colDefs}/>
+          <input type="file" onChange={importExcel} />
+          <MaterialTable title="Import Table" data={data} columns={colDefs} />
         </div>
       </Box>
     </Box>
