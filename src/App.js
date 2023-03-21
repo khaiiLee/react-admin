edc7f7eed711d64adae7f9846621bb84446cfac0
KhaiLee <lequangkhaiuit@gmail.com>
2023-03-21 15:47:08 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
+// import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
@@ -14,11 +14,11 @@ import ImportFile from "./scenes/faq";
 import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 import Sidebar from "./scenes/global/Sidebar";
-import InlineEdit from "./scenes/inlineedit"
+import InlineEdit from "./scenes/inlineedit";
 // import Import from "./scenes/import"
 
 function App() {
-  const [theme, colorMode] = useMode();
+  const [ theme, colorMode ] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
@@ -31,7 +31,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               {/* <Route path="/import" element={<Import/>}/> */}
               <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
@@ -40,7 +40,7 @@ function App() {
               <Route path="/import" element={<ImportFile />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/calendar" element={<Calendar />} />
-              <Route path="/inlineedit" element={<InlineEdit/>}/>
+              <Route path="/inlineedit" element={<InlineEdit />} />
             </Routes>
           </main>
         </div>
