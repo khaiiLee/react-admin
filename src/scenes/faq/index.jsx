f2e7ecf67043516758e066c0a8979abcfdd31e75
KhaiLee <lequangkhaiuit@gmail.com>
2023-03-21 13:44:31 +0700

    commit

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,105 +1,3 @@
-// import { Box } from "@mui/material";
-// import Header from "../../components/Header";
-// import React, { useState } from "react";
-// import MaterialTable from "@material-table/core";
-// import * as XLSX from "xlsx";
-// import axios from "axios";
-
-// const EXTENSIONS = [ "xlsx", "xls", "csv" ];
-
-// const ImportFile = () => {
-//   const [ colDefs, setColDefs ] = useState();
-//   const [ data, setData ] = useState();
-
-//   const getExention = file => {
-//     const parts = file.name.split(".");
-//     const extension = parts[parts.length - 1];
-//     return EXTENSIONS.includes(extension); // return boolean
-//   };
-
-//   const convertToJson = (headers, data) => {
-//     const rows = [];
-//     data.forEach(row => {
-//       let rowData = {};
-//       row.forEach((element, index) => {
-//         rowData[headers[index]] = element;
-//       });
-//       rows.push(rowData);
-//     });
-//     return rows;
-//   };
-
-//   const importExcel = e => {
-//     const file = e.target.files[0];
-//     const reader = new FileReader();
-//     reader.onload = event => {
-//       //parse data
-//       const bstr = event.target.result;
-//       const workBook = XLSX.read(bstr, { type: "binary" });
-
-//       //get first sheet
-//       const workSheetName = workBook.SheetNames[0];
-//       const workSheet = workBook.Sheets[workSheetName];
-//       //convert to array
-//       const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
-//       const headers = fileData[0];
-//       const heads = headers.map(head => ({ title: head, field: head }));
-//       setColDefs(heads);
-
-//       //removing header
-//       fileData.splice(0, 1);
-
-//       setData(convertToJson(headers, fileData));
-
-//       // Push data to server
-//       axios
-//         .post("https://bill-be.onrender.com/insert-mssql", {
-//           query:
-//             "INSERT INTO dbo.uit" +
-//             "(" +
-//             Object.keys(data[0]) +
-//             ")" +
-//             "VALUES" +
-//             "('" +
-//             Object.values(data[0]).join("','") +
-//             "')",
-//           token: 123456,
-//         })
-//         .then(response => {
-//           console.log(response.data);
-//         })
-//         .catch(error => {
-//           console.log(error);
-//         });
-//     };
-
-//     if (file) {
-//       if (getExention(file)) {
-//         reader.readAsBinaryString(file);
-//       } else {
-//         alert("Invalid file input, Select Excel, CSV file");
-//       }
-//     } else {
-//       setData([]);
-//       setColDefs([]);
-//     }
-//   };
-
-//   return (
-//     <Box m="20px">
-//       <Header title="IMPORT" subtitle="Import XLSX or CSV" />
-//       <Box>
-//         <div className="App">
-//           <input type="file" onChange={importExcel} />
-//           <MaterialTable title="Import Table" data={data} columns={colDefs} />
-//         </div>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default ImportFile;
-
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import React, { useState } from "react";
@@ -133,6 +31,7 @@ const ImportFile = () => {
 
   const importExcel = async e => {
     const file = e.target.files[0];
+    const fileName = file.name; // lấy tên file
     if (file && getExtension(file)) {
       const reader = new FileReader();
       reader.onload = async event => {
@@ -149,6 +48,7 @@ const ImportFile = () => {
         const heads = headers.map(head => ({ title: head, field: head }));
         console.log(heads);
         console.log(fileData);
+        console.log(fileName);
         setColDefs(heads);
 
         //removing header
@@ -158,15 +58,26 @@ const ImportFile = () => {
         setData(jsonData);
         // Push data to server
         try {
-          const columns = Object.keys(jsonData[0]);
+          const columns = Object.keys(jsonData[0]).concat(
+            "modifiedOn",
+            "fileName"
+          ); // thêm tên cột modifiedOn vào danh sách các cột
           const values = jsonData
-            .map(item => Object.values(item).join("','"))
-            .join("'),('");
-          const query = `INSERT INTO dbo.uit (${columns}) VALUES ('${values}')`;
+            .map(item =>
+              Object.values(item)
+                .map(value => `'${value}'`)
+                .concat("CURRENT_TIMESTAMP", "'" + fileName + "'")
+                .join(",")
+            )
+            .join("),(");
+          const query = `INSERT INTO dbo.uit (${columns.join(
+            ","
+          )}) VALUES (${values})`;
           const response = await axios.post(
             "https://bill-be.onrender.com/insert-mssql",
             { query, token: 123456 }
           );
+          console.log(query);
           console.log(response.data);
         } catch (error) {
           console.log(error);
